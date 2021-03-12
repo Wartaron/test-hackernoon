28777d5f1d3405781aef39afcc25c82a240d302e
Danilo Plazas <plazasdanilo@hotmail.com>
2021-03-12 00:33:45 -0500

    Webpack config added

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
